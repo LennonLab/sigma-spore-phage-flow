8e040ef8acbccaa552ffb8a39d048d2fbaf5d09f
danschw <danschw@iu.edu>
2021-08-13 10:46:53 -0400

    FCM pipeline transfered to repo and re-run

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # spore-phage-sigma-flow
 Analysis of flow cytometry data relating to aporulation with phage sigma factors
+
+The Pipeline to analyze flow cytometry data of SYBR green stained cultures of Bacillus subtilis is modified from the work of:
+Karava M, Bracharz F, Kabisch J (2019) Quantification and isolation of Bacillus subtilis spores using cell sorting and automated gating. PLoS ONE 14(7): e0219892. https://doi.org/10.1371/journal.pone.0219892
+https://gitlab.com/sporesort/
+
+
+Quantification relies on sample volume measurement by NovCyte machine.
+
+
+To start analysis:
+1. place the flow data, exprtedfrom NovoExpress as fcs files, in the data/FCM folder.
+Experimental data (strain, treatment, sample dilution) are encoded in the names of folders and files.
+
+2. Setting up the environment
+* use R version 3.6.3
+https://support.rstudio.com/hc/en-us/articles/200486138-Changing-R-versions-for-RStudio-desktop
+
+* After switching the R version, open spore-phage-sigma-flow.Rproj in Rsudio
+
+* This pipeline has many dependencies.
+I have used the "renv" package to track all the dependencies.
+learn more at: https://rstudio.github.io/renv/articles/renv.html
+
+First time you will need to install the packages by running the following commands (within project)
+library(renv)
+renv::restore
+
+* I also use the "here" package to call files using relative paths.
+learn more at: https://here.r-lib.org/
+
+
+3. Running the analysis
+
+* from within Rstudio execute the src/quant-spore_veg.R code. Packages and functions used for this analysis are at src/FCM_functions.R.
+
+*Results*
+for each group of replicates the pipeline should make 4 plots in figs/gate_plots folder:
+> singlet: FSC height vs. area. used to exclude doublets
+> noise: distribution if FSC are values, with line marking the noise filtering threshold.
+> scatterNoise: events scatter plot on SYBR fluorescence vs SSC area with events filtered out by noise marked in gray, and events passed to clustering in blue.
+> cluster: Events scatter plot on SYBR fluorescence vs FSC area with assignment to spore and veg clusters given by color. Blue dots represent the model based cluster center for veg and spore.
+
+The actual counts. along with pipeline values collected along the way, are saved to data/output as .csv files.
+
+
+ 
+
+
+
---
+++ src/quant-spore_veg.R
@@ -0,0 +1,278 @@
+## Coevolution with a seed bank - analysis of sporulation in evolved lines
+library(renv)
+# # # renv::init()
+# renv::restore()
+library(here)
+#analysis of flow-cytometry population data using Karava's analysis.R as reference.
+source(here("src/FCM_functions.R"))
+set.seed(1)
+#point to data
+   all.folders <- 
+   list.dirs(here("data/FCM/"),recursive = F)
+
+for (current_folder in all.folders){
+   
+   # day is an experimental batch
+   day <- str_extract(current_folder, "FCM/.*$") %>% 
+      str_remove("FCM/")
+
+   folders <- list.dirs(current_folder)
+   # remove the folder of folders
+   if(length(folders) > 1){
+      folders <- folders[!str_detect(folders, paste0(day,"$"))]
+   }
+   # place "xIPTG" first" to be used in making of stran model
+   tmp <- c(folders[grep("xIPTG", folders)], folders[grep("xIPTG", folders, invert = TRUE)])
+   folders <- tmp
+   rm(tmp)
+   
+   # Make directories to store the data
+   if (! dir.exists(here("fig/gate_plots", day))){
+      dir.create(here("fig/gate_plots", day), recursive = T)
+   }
+   
+   if (! dir.exists(here("data/output", day))){
+      dir.create(here("data/output", day), recursive = T)
+   }
+   #fields to parse from the .fcs file name (separated by underscore and/or hyphen)
+   sample.var <- c("strain","media","treat","dilution","well","colony" ,"rep","xt","num") 
+   
+   # the following loop goes over each replicated samples and does the actual anaysis
+   for (folder in folders){
+      #### Load data, sample set ####
+      fcsset <- flowCreateFlowSet(filepath = folder, sample_variables = sample.var,
+                                  transformation = FALSE,separators = "[-_\\.]")
+      #transform with arcsine, recomendded by Karava et al.
+      fcsset <- Transform.Novocyte(fcsset)
+      
+      #data frame to collect stats
+      df.stats <- fcsset%>%
+         flowFcsToDf(.)%>%
+         select(.,sample.var)%>%
+         distinct()
+      df.stats$exp <- day
+      df.stats$total.events <- NA
+      df.stats$volume.nL <- NA
+      df.stats$limit.events <- NA
+      df.stats$limit.volume.uL <- NA
+      df.stats$singlets <- NA
+      df.stats$neg.rmv <- NA
+      df.stats$noise.cutoff <- NA
+      
+      
+      #### Gating for singlets with flowStats ####
+      
+      # The gate function needs to be applied to each sample separately
+      # get number of samples
+      n.sample <- nrow(fcsset@phenoData@data)
+      
+      
+      
+      #initialise list to store singlet plots
+      plot.list <- vector('list', n.sample)
+      
+      for (i in 1:n.sample){
+         # collect metadata for stats table
+         df.stats$volume.nL[i] <- as.numeric(fcsset[[i]]@description$`$VOL`)
+         df.stats$total.events[i] <- as.numeric(fcsset[[i]]@description$`$TOT`)
+         df.stats$limit.volume.uL[i] <- as.numeric(fcsset[[i]]@description$`#NCVolumeLimits`)
+         df.stats$limit.events[i] <- as.numeric(fcsset[[i]]@description$`#NCEventsLimits`)
+         
+         
+         singlet_gate <- gate_singlet(fcsset[[i]], area = "asinh.FSC.A", height = "asinh.FSC.H", filterId = "Singlets",wider_gate = TRUE )
+         df.stats$singlets[i] <- summary(filter(fcsset[[i]],singlet_gate))@true
+         
+         #plot gate
+         id <- fcsset[[i]]@description$GUID
+         plot.list[[i]] <-
+            as.ggplot(
+               ggcyto(fcsset[[i]], aes(x = `asinh.FSC.A`, y =  `asinh.FSC.H`))+
+                  geom_hex(bins = 500)+
+                  geom_gate(singlet_gate, size=0.01)+
+                  geom_stats(adjust=c(0.2,0.75))+
+                  theme_cowplot(font_size = 8)+
+                  scale_y_continuous(labels =  function(x) format(x, scientific = TRUE))+
+                  scale_x_continuous(labels =  function(x) format(x, scientific = TRUE))+
+                  facet_null()+
+                  ggtitle(df.stats$well[i])
+               
+            )
+         
+         #apply gate
+         fcsset[[i]] <- fcsset[[i]] %>%
+            Subset(singlet_gate)
+         
+         
+         # filter negatives
+         neg.gate <- rectangleGate("asinh.BL1.A" = c(0, 15), "asinh.FSC.A" = c(0, 15),"asinh.SSC.A" = c(0, 15))
+         df.stats$neg.rmv[i] <-  df.stats$singlets[i]- summary(filter(fcsset[[i]],neg.gate))@true
+         #apply gate
+         fcsset[[i]] <- fcsset[[i]] %>%
+            Subset(neg.gate) # remove negative
+         
+         #find cutoff to remove noise by FSC
+         noise<- rangeGate(x = fcsset[[i]], "asinh.FSC.A", alpha=0.5,sd=3, absolute = F)
+         df.stats$noise.cutoff[i] <- noise@min[[1]]
+         
+      }
+      
+      #save plot
+      ggsave2(filename = paste0(fcsset[[i]]@description$`$SRC`,".pdf" ), 
+              plot = plot_grid(plotlist = plot.list),
+              path = here("fig/gate_plots", day))
+      
+      
+      # plotting noise gate
+      
+      noise.plot <-   
+         ggcyto(fcsset,aes(asinh.FSC.A))+
+         geom_density(fill="grey80")+
+         geom_vline(data = df.stats, aes(xintercept=noise.cutoff), color="red")+
+         facet_wrap(~well)+theme_cowplot()
+      
+      ggsave2(filename = paste0("noise_",fcsset[[i]]@description$`$SRC`,".pdf" ), 
+              plot = noise.plot,
+              path = here("fig/gate_plots/", day))
+      
+      #### transform to dataframe ####
+      df.set <- fcsset%>%
+         flowFcsToDf(.)
+      # no. of events before noise filter
+      df.stats <- 
+         df.set %>%
+         group_by(well) %>%
+         summarise(noisy.events=n())%>%
+         full_join(df.stats,.)
+      
+      
+      
+      # remove noise
+      clean.df <- df.set[0,]
+      wells <- levels(as.factor(df.stats$well))
+      for (wl in wells){
+         clean.df <- rbind(clean.df,
+                           df.set %>%
+                              dplyr::filter(well==wl)%>%
+                              dplyr::filter(asinh.FSC.A>df.stats$noise.cutoff[df.stats$well==wl]))
+      }
+      
+      
+      df.stats <- 
+         clean.df %>%
+         group_by(well) %>%
+         summarise(clean.events=n())%>%
+         full_join(df.stats,.)
+      
+      complot <- 
+         ggplot(df.set, aes(asinh.SSC.A, asinh.BL1.A))+
+         geom_point(size=0.1, color="grey")+
+         geom_point(data=clean.df,size=0.1, color="blue")+
+         geom_density2d(col = "white",  size = 0.1, alpha = 0.5) +
+         scale_alpha_continuous(guide = FALSE) +
+         facet_wrap(~well)+
+         theme_cowplot()+
+         panel_border()
+      
+      ggsave2(filename = paste0("scatterNoise_",fcsset[[i]]@description$`$SRC`,".png" ), 
+              plot = complot,
+              path = here("fig/gate_plots/", day))
+      #### predict centers of sub-populations ####
+      
+      # I pre-compiled a model for Bacillus subtilis TS01
+      # base::load("data/cluster_models/TS01_cluster_model.Rdata")
+      # # use this to generate prediction model from data frame of events
+      # df.mix <- clean.df%>%
+      #       dplyr::select(asinh.FSC.A,asinh.BL1.A)%>%
+      #       as.matrix() %>%
+      #       mclust::Mclust(data = ., G = 2) #I  have only 2 clusters
+      # save(df.mix, file="data/cluster_models/name_cluster_model.Rdata")
+      
+      current.strain <- paste(df.set$strain[1],day, sep = "_")
+      models <- list.files("data/cluster_models/", full.names = T)
+      
+      
+      if (!any(grepl(current.strain,models))&
+          !grepl("blank",current.strain)){
+         # generate cluster prediction model for strain from current set
+         df.mix <- clean.df%>%
+            # dplyr::filter(phage=="noPHI")%>%
+            dplyr::select(asinh.FSC.A,asinh.BL1.A)%>%
+            as.matrix() %>%
+            mclust::Mclust(data = ., G = 2) #I  have only 2 clusters
+         save(df.mix, file=paste0("data/cluster_models/",current.strain,"_cluster_model.Rdata")) 
+         model.use <- paste("new for ", current.strain)
+      } else{
+         if (grepl("blank",current.strain)){
+            base::load("data/cluster_models/TS01_cluster_model.Rdata")
+            model.use <- "TS01"
+         } else{
+            # load previously compiled prediction model for strain
+            model.use <- models[grep(current.strain,models)]
+            base::load(model.use)
+         }
+      }
+      df.stats$clust.model <- model.use
+      
+      # getting centers for visualization and export
+      centers.list.df <- t(df.mix$parameters$mean)
+      
+      # assigning cluster to population based on SYBR fluoresence (BL1)
+      # higher SYBR => veg cell
+      pop.tbl <- data.frame(cluster=c(1,2), pop=NA)
+      pop.tbl$pop[which.max(centers.list.df[,"asinh.BL1.A"])] <- "veg"
+      pop.tbl$pop[which.min(centers.list.df[,"asinh.BL1.A"])] <- "spore"
+      
+      ### Prediction of clusters for all samples ####
+      cluster.predict <- clean.df %>%
+         dplyr::select( asinh.FSC.A, asinh.BL1.A) %>%
+         predict.Mclust(df.mix,.)
+      
+      clean.df$pop <- sapply(cluster.predict$classification,  function(x) {ifelse(x==pop.tbl$cluster[1], pop.tbl$pop[1],pop.tbl$pop[2])})
+      
+      p <-       
+         ggplot(clean.df, aes(asinh.FSC.A, asinh.BL1.A)) +
+         geom_hex(aes(fill = pop), bins = 300) + # ,alpha=..ncount.. #order= ?
+         geom_density2d(color="black",  size = 0.1) +
+         theme_bw()+ 
+         geom_point(aes(centers.list.df[1, 1], centers.list.df[1, 2]), col = "blue", size = 1) +
+         geom_point(aes(centers.list.df[2, 1], centers.list.df[2, 2]), col = "blue", size = 1) +
+         facet_wrap(~well)
+
+         ggsave2(filename = paste0("cluster_",fcsset[[i]]@description$`$SRC`,".png" ), 
+              plot = p,
+              path = here("fig/gate_plots", day))
+      
+      #### Get the quantities ####
+      
+      clust.count <- data.frame(clean.df, cluster = cluster.predict$classification) %>%
+         group_by(well, cluster) %>%
+         summarize(count = n()) 
+      for(i in pop.tbl$cluster){
+         clust.count$cluster[clust.count$cluster %in%  pop.tbl$cluster[i]] <-  pop.tbl$pop[i]
+      }
+      
+      df.stats <-    
+         spread(clust.count ,cluster, count)%>%
+         full_join(df.stats,.)
+      
+
+      # calculate concentrations based on volume and dilution
+      df.stats$spore.ml <- as.numeric(sapply(strsplit(df.stats$dilution,"x"), "[[", 2))* #dilution factor
+         df.stats$spore/(df.stats$volume.nL/1e6)
+      df.stats$veg.ml <- as.numeric(sapply(strsplit(df.stats$dilution,"x"), "[[", 2))* #dilution factor
+         df.stats$veg/(df.stats$volume.nL/1e6)  
+      
+      # write results to file
+      write_csv(df.stats,
+                path = here("data/output/",day,paste0(fcsset[[i]]@description$`$SRC`,".csv")))
+      
+      print(paste("done",folder))
+   } #folder loop
+   
+} # experimental batch loop     
+
+
+
+
+
+
